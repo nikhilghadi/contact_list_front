ce7158f58b6765094165af16228cd72d8cda855d
Nikhil <nikhilghadi665@gmail.com>
2022-05-01 01:00:04 +0530

    cors

--- src/Components/index.js
+++ src/Components/index.js
@@ -34,6 +34,10 @@ export class index extends Component {
       axios({
             method: 'GET',
             url: 'https://glacial-hamlet-02663.herokuapp.com/search_phone_number/',
+            mode: 'no-cors',
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            },
             params: {
               text:this.state.input_text
             }
