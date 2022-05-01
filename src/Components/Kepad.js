22d980604be8a9fe4c2d0a5a59723670074395da
Nikhil <nikhilghadi665@gmail.com>
2022-05-01 17:44:30 +0530

    aadd

--- src/Components/Kepad.js
+++ src/Components/Kepad.js
@@ -13,18 +13,18 @@ export class Kepad extends Component {
 
             <p>
             <button className="key" onClick={this.pressButton} name="1" data-number="1">1 </button>
-            <button className="key" onClick={this.pressButton} name="2" data-number="2">2 </button>
-            <button className="key" onClick={this.pressButton} name="3" data-number="3">3 </button>
+            <button className="key" onClick={this.pressButton} name="2" data-number="2">2 <small>abc</small></button>
+            <button className="key" onClick={this.pressButton} name="3" data-number="3">3 <small>def</small></button>
             </p>
             <p>
-            <button className="key" onClick={this.pressButton} name="4" data-number="4">4 </button>
-            <button className="key" onClick={this.pressButton} name="5" data-number="5">5 </button>
-            <button className="key" onClick={this.pressButton} name="6" data-number="6">6 </button>
+            <button className="key" onClick={this.pressButton} name="4" data-number="4">4 <small>ghi</small></button>
+            <button className="key" onClick={this.pressButton} name="5" data-number="5">5 <small>jkl</small></button>
+            <button className="key" onClick={this.pressButton} name="6" data-number="6">6 <small>mno</small></button>
             </p>
             <p>
-            <button className="key" onClick={this.pressButton} name="7" data-number="7">7</button>
-            <button className="key" onClick={this.pressButton} name="8" data-number="8">8 </button>
-            <button className="key" onClick={this.pressButton} name="9" data-number="9">9 </button>
+            <button className="key" onClick={this.pressButton} name="7" data-number="7">7<small>pqrs</small></button>
+            <button className="key" onClick={this.pressButton} name="8" data-number="8">8<small>tuv</small> </button>
+            <button className="key" onClick={this.pressButton} name="9" data-number="9">9 <small>wxyz</small></button>
             </p>
             <p>
             <button className="key" onClick={this.pressButton} name=" 0" data-number="0">0 </button>
